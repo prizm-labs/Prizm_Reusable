62ea89aa5af933f4d41d412fc09b18c353e89baf
taylorjames9 <taylorjames9@gmail.com>
2016-03-23 17:18:36 -0700

    example scene for draggable snappable pieces

---
+++ Assets/Scripts/SnapToPosition/Movable_Snap_Piece.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movable_Snap_Piece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SnapToPosition/Placement_Spot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Placement_Spot : MonoBehaviour {
+
+
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.green;
+	public float duration = 1.0F;
+	public Renderer rend;
+	public bool pieceOnMe;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+	void Update() {
+		if (!pieceOnMe) {
+			float lerp = Mathf.PingPong (Time.time, duration) / duration;
+			rend.material.color = Color.Lerp (colorStart, colorEnd, lerp);
+		}
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.GetComponent<Movable_Snap_Piece> () != null) {
+			Debug.Log ("touching a piece that is important to me");
+			pieceOnMe = true;
+			other.transform.position = new Vector3 (transform.position.x, transform.position.y+0.1f, transform.position.z);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.GetComponent<Movable_Snap_Piece> () != null) {
+			pieceOnMe = false;
+
+		}
+	}
+
+}
